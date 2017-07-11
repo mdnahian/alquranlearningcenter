d3055a5535f0837b17cca0c64a83343fda763f30
Md Islam <mdnahian@outlook.com>
2017-07-11 14:43:55 -0400

    updated landing page

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_login import *
 from flask_socketio import SocketIO
-from User import User
 import os
-import model
 from datetime import datetime, timedelta
 
 
@@ -14,39 +12,23 @@ login_manager.init_app(app)
 socketio = SocketIO(app)
 
 
-def get_next_session_from_sessions(sessions, uid='uid_student'):
-    next_session = None
-    # print sessions
-    for session in sessions:
-        user = User().get(session.get(uid))
-        if user is not None:
-            session['user'] = vars(user)
+# def get_next_session_from_sessions(sessions, uid='uid_student'):
+#     next_session = None
+#     for session in sessions:
+#         user = User().get(session.get(uid))
+#         if user is not None:
+#             session['user'] = vars(user)
 
-            d1 = datetime.strptime(session.get('datetime'), "%A %I:%M%p")
-            d2 = d1 + timedelta(hours=int(session.get('duration')))
-            current_d = datetime.now()
+#             d1 = datetime.strptime(session.get('datetime'), "%A %I:%M%p")
+#             d2 = d1 + timedelta(hours=int(session.get('duration')))
+#             current_d = datetime.now()
             
-            # check if same day
-            if session.get('datetime').split(' ')[0].strip() == current_d.strftime("%A"):
-                # check if within time frame
-                if current_d.time() >= d1.time() and current_d.time() <= d2.time():
-                    next_session = session
-    return next_session
-
-
-# @app.context_processor
-# def override_url_for():
-#     return dict(url_for=dated_url_for)
-
-
-# def dated_url_for(endpoint, **values):
-#     if endpoint == 'static':
-#         filename = values.get('filename', None)
-#         if filename:
-#             file_path = os.path.join(app.root_path,
-#                                      endpoint, filename)
-#             values['q'] = int(os.stat(file_path).st_mtime)
-#     return url_for(endpoint, **values)
+#             # check if same day
+#             if session.get('datetime').split(' ')[0].strip() == current_d.strftime("%A"):
+#                 # check if within time frame
+#                 if current_d.time() >= d1.time() and current_d.time() <= d2.time():
+#                     next_session = session
+#     return next_session
 
 
 @login_manager.user_loader
@@ -57,8 +39,10 @@ def load_user(user_id):
 @app.route('/')
 def index():
     if current_user.is_authenticated:
-        return redirect('/dashboard') 
-    return render_template('template.html', page='pages/index.html')
+        return redirect('/dashboard')
+    num_teachers = 0
+    num_students = 0
+    return render_template('template.html', page='pages/index.html', num_students=str(num_students), num_teachers=str(num_teachers))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -87,23 +71,25 @@ def signup():
 @login_required
 def dashboard():
     if current_user.account_type == 'teacher':
-        sessions = model.Session(uid_teacher=current_user.uid).get_from_id(multiple=True)
-        next_session = get_next_session_from_sessions(sessions)
+        # sessions = model.Session(uid_teacher=current_user.uid).get_from_id(multiple=True)
+        # next_session = get_next_session_from_sessions(sessions)
         return render_template('template.html', page='pages/dash_teacher.html', sessions=sessions, next_session=next_session)
     elif current_user.account_type == 'student':
-        sessions = model.Session(uid_student=current_user.uid).get_from_id(multiple=True)
-        next_session = get_next_session_from_sessions(sessions, uid='uid_teacher')
+        # sessions = model.Session(uid_student=current_user.uid).get_from_id(multiple=True)
+        # next_session = get_next_session_from_sessions(sessions, uid='uid_teacher')
         return render_template('template.html', page='pages/dash_student.html', next_session=next_session)
     elif current_user.account_type == 'admin':
-        sessions = model.Session().get_from_id(get_all=True)
+        # sessions = model.Session().get_from_id(get_all=True)
         return render_template('template.html', page='pages/dash_admin.html', sessions=sessions)
     else:
         return redirect('logout')
+
 
 @app.route('/account')
 @login_required
 def account():
     return render_template('template.html', page='pages/account.html')
+
 
 @app.route('/call/<sid>')
 @login_required
